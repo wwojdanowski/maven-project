86e2030d573d43a6de6a83aae59824aa62f425a0
wwojdanowski <32903289+wwojdanowski@users.noreply.github.com>
2018-03-04 12:46:57 +0100

    Add javadoc comment for greet method.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  /**
+  * @param someone the name of a person
+  * @return greeting string
+  */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
